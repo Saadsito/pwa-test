2dfd84fa210aeec1a65354717238b4708a501bd8
Karen Morán <kamoran.19@est.ucab.edu.ve>
2022-05-22 12:40:28 -0400

    add config firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,15 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDDtbzm-dUrh0v5ZylVFm_NJRbWNZPCFLI",
+  authDomain: "zuli-app.firebaseapp.com",
+  projectId: "zuli-app",
+  storageBucket: "zuli-app.appspot.com",
+  messagingSenderId: "112603494268",
+  appId: "1:112603494268:web:8ea6a0a43dd8ee55cf56f9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
